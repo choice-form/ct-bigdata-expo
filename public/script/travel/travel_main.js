1fc28b52c9bfbc15999ccdb93cd650778b45afb5
ShengXX <shengxx@suruey.best>
2015-10-29 18:15:02 +0800

    #travel main js 更新

--- public/script/travel/travel_main.js
+++ public/script/travel/travel_main.js
@@ -1,11 +1,9 @@
 /**
  * Created by JaniseSheng on 15-10-28.
  */
-var Travel_Date;//市区
-
 var questAjax = function (data,callback) {
   $.ajax({
-    url: "/realty",
+    url: "/travel",
     data: data,
     type: "post",
     dataType: "json",
@@ -20,58 +18,15 @@ var questAjax = function (data,callback) {
   })
 }
 
-
-
-//设置线上热门搜索
-var Travel_HotkeySelect_screen_a  = function(){
+var Travel_SetAllCityInfo_screen_a = function(){
   var setInfo = function(res){
-    var info =  res;
-    console.log(info);
-    var html = template('js-tpl-travel-screen-a', {item:info});
-    document.getElementById('js-travel-screen-a').innerHTML = html;
-  }
-  questAjax({
-    fun: "hotKeyword"
-  },setInfo);
-}
+    console.log(res)
+  };
 
-var clickEffect = {
-  'district': function () {
-    $('.district').click(function () {
-      var addressValue = $(this).attr("name");
-      $('.district').removeClass('active');
-      $(this).addClass('active');
-
-      $('.screen_toggle').removeClass('active');
-      $(addressValue).addClass('active');
-
-      House_addresstext = $(this).text();
-
-      if(House_addresstext=='全市'){
-        House_SetAllCityInfo_screen_a();
-      }else {
-        House_SetAreaInfo_screen_a(House_addresstext);
-      }
-    });
-
-  },
-
-  'row': function () {
-    $('.row').click(function(){
-      $('#tab-1 .row').removeClass('active');
-      $(this).addClass('active');
-      var code = $(this).attr('date-id');
-      console.log(code);
-      House_SetAreaInfo_screen_b(code);
-    })
-  }
+  questAjax({fun:'hotKeyword'},setInfo);
 }
 
 $(document).ready(function () {
-  //clickEffect.district();
-
-  //页面加载模式获取全市房产数据
-  Travel_HotkeySelect_screen_a();
+  Travel_SetAllCityInfo_screen_a();
 });
-
 
