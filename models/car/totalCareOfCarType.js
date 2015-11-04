cec91c0e573021cb492a4c13657a8710601c99e9
yyong <coolfish2004@hotmail.com>
2015-11-04 17:59:58 +0800

    修改

---
+++ models/car/totalCareOfCarType.js
@@ -0,0 +1,81 @@
+// 全市关心6类车型的小区top10
+// 请求: type 电动车/高端车/SUV/中小型车/跑车/商务车
+// 返回: 数量从高到低top10
+var totalCareOfcarType = {
+	code: "000",
+	msg: "success",
+	result: [
+		{
+			time: "50周",
+			villagecode: 215032432,
+			villagename: "徐汇街道",
+			villagerank: 1,
+			villagecareofcarnum: 242435
+		},
+		{ 
+			time: "50周",
+			villagecode: 215032432,
+			villagename: "闵行街道",
+			villagerank: 2,
+			villagecareofcarnum: 242435
+		},
+		{
+			time: "50周",
+			villagecode: 215032432,
+			villagename: "闵行街道",
+			villagerank: 2,
+			villagecareofcarnum: 242435
+		},
+		{
+			time: "50周",
+			villagecode: 215032432,
+			villagename: "闵行街道",
+			villagerank: 2,
+			villagecareofcarnum: 242435
+		},
+		{
+			time: "50周",
+			villagecode: 215032432,
+			villagename: "闵行街道",
+			villagerank: 2,
+			villagecareofcarnum: 242435
+		},
+		{
+			time: "50周",
+			villagecode: 215032432,
+			villagename: "徐汇街道",
+			villagerank: 1,
+			villagecareofcarnum: 242435
+		},
+		{
+			time: "50周",
+			villagecode: 215032432,
+			villagename: "闵行街道",
+			villagerank: 2,
+			villagecareofcarnum: 242435
+		},
+		{
+			time: "50周",
+			villagecode: 215032432,
+			villagename: "闵行街道",
+			villagerank: 2,
+			villagecareofcarnum: 242435
+		},
+		{
+			time: "50周",
+			villagecode: 215032432,
+			villagename: "闵行街道",
+			villagerank: 2,
+			villagecareofcarnum: 242435
+		},
+		{
+			time: "50周",
+			villagecode: 215032432,
+			villagename: "闵行街道",
+			villagerank: 2,
+			villagecareofcarnum: 242435
+		}
+	]
+}
+
+module.exports = totalCareOfcarType;
